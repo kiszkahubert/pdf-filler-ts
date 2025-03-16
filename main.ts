4f6352de4eb2b8613945100b5c229cd0dbe88dc6
Hubert <kiszkahub@gmail.com>
2025-03-16 22:40:47 +0100

    ..

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-console.log("works");
+import * as fs from 'fs';
+import { PDFDocument } from 'pdf-lib';
+
+const filePath = 'test_files/04.csv';
+
+function readCSVData(): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data.split('\n'));
+        });
+    });
+}
+
+async function processCSV(){
+    try{
+        const rows = await readCSVData();
+        rows.forEach(row =>{
+            const cols = row.split(',');
+            console.log(cols);
+        })
+    } catch (err){
+        console.log(err)
+    }
+}
+
+processCSV();
